79f48e54219e8d1893fecdb8db7253e148dd4b1a
Arthur Hammer <hi@arthurhammer.de>
2018-09-07 16:37:47 +0200

    Apply library updates to albums incrementally
    Reduces time until most updates are visible to user by orders of magnitude.

--- Frame Grabber/Model/Album.swift
+++ Frame Grabber/Model/Album.swift
@@ -1,6 +1,10 @@
 import Photos
 
-protocol Album {
+protocol PhotosIdentifiable {
+    var id: String { get }
+}
+
+protocol Album: PhotosIdentifiable {
     var assetCollection: PHAssetCollection { get }
     var title: String? { get }
     var count: Int { get }
@@ -8,8 +12,16 @@ protocol Album {
 }
 
 extension Album {
+    var id: String {
+        return assetCollection.localIdentifier
+    }
+
     var title: String? {
         return assetCollection.localizedTitle
+    }
+
+    var isEmpty: Bool {
+        return count == 0
     }
 }
 
