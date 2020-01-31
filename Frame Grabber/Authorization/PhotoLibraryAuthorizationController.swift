153b5eee655d877594fd9d89e3857234ff48e031
Arthur Hammer <hi@arthurhammer.de>
2020-01-31 22:54:48 +0100

    Update authorization UI

--- Frame Grabber/Authorization/PhotoLibraryAuthorizationController.swift
+++ Frame Grabber/Authorization/PhotoLibraryAuthorizationController.swift
@@ -2,8 +2,6 @@ import UIKit
 import Photos
 import SafariServices
 
-// TODO: Landscape
-
 class PhotoLibraryAuthorizationController: UIViewController {
 
     static var needsAuthorization: Bool {
@@ -12,12 +10,14 @@ class PhotoLibraryAuthorizationController: UIViewController {
 
     var didAuthorizeHandler: (() -> ())?
 
-    @IBOutlet private var statusView: StatusView!
+    @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var messageLabel: UILabel!
+    @IBOutlet private var button: UIButton!
     @IBOutlet private var privacyButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateViews()
+        configureViews()
     }
 
     @IBAction private func requestAuthorization() {
@@ -35,30 +35,33 @@ class PhotoLibraryAuthorizationController: UIViewController {
         present(SFSafariViewController(url: url), animated: true)
     }
 
-    private func updateViews() {
+    private func configureViews() {
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .title1, size: 36, weight: .semibold)
+
+        button.tintColor = .white
+        button.backgroundColor = Style.Color.mainTint
+        button.layer.cornerRadius = Style.Size.buttonCornerRadius
+        button.titleLabel?.adjustsFontForContentSizeCategory = true
+        button.titleLabel?.minimumScaleFactor = 0.8
+
         privacyButton.titleLabel?.adjustsFontForContentSizeCategory = true
         privacyButton.titleLabel?.minimumScaleFactor = 0.8
-        statusView.button.titleLabel?.adjustsFontForContentSizeCategory = true
-        statusView.button.titleLabel?.minimumScaleFactor = 0.8
-        
-        statusView.message = message(for: PHPhotoLibrary.authorizationStatus())
+
+        updateViews()
     }
 
-    private func message(for status: PHAuthorizationStatus) -> StatusView.Message? {
-        let title = NSLocalizedString("authorization.title", value: "Frame Grabber 👋", comment: "Photo library authorization title")
+    private func updateViews() {
+        titleLabel.text = NSLocalizedString("authorization.title", value: "Welcome to\nFrame Grabber", comment: "Photo library authorization title")
 
-        switch status {
-
+        switch PHPhotoLibrary.authorizationStatus() {
         case .denied, .restricted:
-            return .init(title: title,
-                         message: NSLocalizedString("authorization.deniedMessage", value: "Frame Grabber extracts full-resolution video frames as images. You can allow access to your videos in Settings.", comment: "Photo library authorization denied message"),
-                         action: NSLocalizedString("authorization.deniedAction", value: "Open Settings", comment: "Photo library authorization denied action"))
+            messageLabel.text = NSLocalizedString("authorization.deniedMessage", value: "Frame Grabber extracts full-resolution video frames as images. You can allow access to your videos in Settings.", comment: "Photo library authorization denied message")
+            button.setTitle(NSLocalizedString("authorization.deniedAction", value: "Open Settings", comment: "Photo library authorization denied action"), for: .normal)
 
         // Mostly for `notDetermined` but also as fallback if we land in `authorized` state.
         default:
-            return .init(title: title,
-                         message: NSLocalizedString("authorization.notDeterminedMessage", value: "Frame Grabber extracts full-resolution video frames as images. Get started by allowing access to your videos.", comment: "Photo library authorization default message"),
-                         action: NSLocalizedString("authorization.notDeterminedAction", value: "Allow Access", comment: "Photo library authorization default action"))
+            messageLabel.text = NSLocalizedString("authorization.notDeterminedMessage", value: "Frame Grabber extracts full-resolution video frames as images. Get started by allowing access to your videos.", comment: "Photo library authorization default message")
+            button.setTitle(NSLocalizedString("authorization.notDeterminedAction", value: "Get Started", comment: "Photo library authorization default action"), for: .normal)
         }
     }
 }
