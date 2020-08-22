b2cbfd56961cc4d0c8c1e2326de6f1178f25f0f6
Arthur Hammer <hi@arthurhammer.de>
2020-08-22 10:27:09 +0200

    Update dark accent color

--- Frame Grabber/Application/Style.swift
+++ Frame Grabber/Application/Style.swift
@@ -7,7 +7,7 @@ struct Style {
         static let mainTint: UIColor = .init { traitCollection in
             (traitCollection.userInterfaceStyle == .light)
                 ? UIColor(red: 0.27, green: 0.16, blue: 0.97, alpha: 1.00)
-                : .systemTeal
+                : UIColor(red: 0.00, green: 0.77, blue: 0.81, alpha: 1.00)
         }
 
         static let secondaryTint: UIColor = .init { traitCollection in
