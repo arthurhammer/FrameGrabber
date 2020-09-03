6ec5b245bbe56ccdaed340db464a72b163ccc72e
Arthur Hammer <hi@arthurhammer.de>
2020-09-03 09:37:04 +0200

    Redesign about screen

--- Frame Grabber/About/AboutViewController.swift
+++ Frame Grabber/About/AboutViewController.swift
@@ -3,31 +3,21 @@ import MessageUI
 import SafariServices
 import UIKit
 
-class AboutTableViewController: UITableViewController, MFMailComposeViewControllerDelegate {
+class AboutViewController: UITableViewController, MFMailComposeViewControllerDelegate {
+
+    enum Section: Int {
+        case featured
+        case about
+    }
 
     let app = UIApplication.shared
     let bundle = Bundle.main
     let device = UIDevice.current
 
-    var hasPurchased: Bool {
-        paymentsManager.hasPurchasedProduct(withId: inAppPurchaseId)
-    }
-
-    private let paymentsManager = StorePaymentsManager.shared
-    private let inAppPurchaseId = About.inAppPurchaseIdentifier
-    private let reviewURL = About.storeReviewURL
-    private let contactSubject = UserText.aboutContactSubject
-
-    private lazy var contactMessage = """
-    \n\n
-    \(bundle.longFormattedVersion)
-    \(device.systemName) \(device.systemVersion)
-    \(device.type ?? device.model)
-    """
+    private let topMargin: CGFloat = 12
 
     @IBOutlet private var rateButton: UIButton!
-    @IBOutlet private var iceCreamButton: UIButton!
-    @IBOutlet private var versionLabel: UILabel!
+    @IBOutlet private var purchaseButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,18 +26,23 @@ class AboutTableViewController: UITableViewController, MFMailComposeViewControll
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        updateViews()
+
+        transitionCoordinator?.animate(alongsideTransition: { [weak self] _ in
+            self?.updateNavigationBar()
+        }, completion: { [weak self] _ in
+            self?.updateNavigationBar()
+        })
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
 
-        let sendFeedpackPath = IndexPath(row: 0, section: 1)
+        let contactPath = IndexPath(row: 0, section: 1)
         let privacyPolicyPath = IndexPath(row: 1, section: 1)
         let showSourceCodePath = IndexPath(row: 2, section: 1)
 
         switch indexPath {
-        case sendFeedpackPath: sendFeedback()
+        case contactPath: contact()
         case privacyPolicyPath: showPrivacyPolicy()
         case showSourceCodePath: showSourceCode()
         default: break
@@ -59,44 +54,73 @@ class AboutTableViewController: UITableViewController, MFMailComposeViewControll
     }
 
     private func configureViews() {
-        tableView.backgroundColor = .clear
-
         rateButton.layer.cornerRadius = Style.buttonCornerRadius
         rateButton.layer.cornerCurve = .continuous
 
-        versionLabel.text = String.localizedStringWithFormat(UserText.aboutVersionFormat, bundle.shortFormattedVersion)
-
-        updateViews()
+        purchaseButton.backgroundColor = UIColor.accent?.withAlphaComponent(0.1)
+        purchaseButton.layer.cornerRadius = Style.buttonCornerRadius
+        purchaseButton.layer.cornerCurve = .continuous
     }
 
-    private func updateViews() {
-        let title = hasPurchased ? UserText.aboutPurchasedButton : UserText.aboutNotPurchasedButton
-        iceCreamButton.setTitle(title, for: .normal)
+    private func updateNavigationBar() {
+        let bar = navigationController?.navigationBar
+        bar?.tintColor = nil
+        bar?.shadowImage = nil
+        bar?.setBackgroundImage(nil, for: .default)
+    }
+
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        switch Section(section) {
+        case .about:
+            return String.localizedStringWithFormat(UserText.aboutVersionFormat, bundle.shortFormattedVersion)
+        default:
+            return nil
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch Section(section) {
+        case .featured:
+            return topMargin
+        default:
+            return UITableView.automaticDimension
+        }
     }
 }
 
 // MARK: - Actions
 
-extension AboutTableViewController {
+extension AboutViewController {
+
+    @IBAction private func done() {
+        dismiss(animated: true)
+    }
 
     @IBAction private func rate() {
-        guard let url = reviewURL,
+        guard let url = About.storeReviewURL,
             app.canOpenURL(url) else { return }
 
         app.open(url)
     }
     
-    func sendFeedback() {
+    private func contact() {
         guard MFMailComposeViewController.canSendMail() else {
             presentAlert(.mailNotAvailable(contactAddress: About.contactAddress))
             return
         }
 
+        let contactMessage = """
+        \n\n
+        \(bundle.longFormattedVersion)
+        \(device.systemName) \(device.systemVersion)
+        \(device.type ?? device.model)
+        """
+
         let mailController = MFMailComposeViewController()
         mailController.view.tintColor = .accent
         mailController.mailComposeDelegate = self
         mailController.setToRecipients([About.contactAddress])
-        mailController.setSubject(contactSubject)
+        mailController.setSubject(UserText.aboutContactSubject)
         mailController.setMessageBody(contactMessage, isHTML: false)
 
         present(mailController, animated: true)
@@ -106,12 +130,12 @@ extension AboutTableViewController {
         dismiss(animated: true)
     }
 
-    func showSourceCode() {
+    private func showSourceCode() {
         guard let url = About.sourceCodeURL else { return }
         showURL(url)
     }
 
-    func showPrivacyPolicy() {
+    private func showPrivacyPolicy() {
         guard let url = About.PrivacyPolicy.preferred else { return }
         showURL(url)
     }
