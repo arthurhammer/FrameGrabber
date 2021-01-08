306fc14970d17e52fd24e3a0b127f28c4ff28e82
Arthur Hammer <hi@arthurhammer.de>
2021-01-08 18:42:34 +0100

    Add support for make, model and other metadata

--- Frame Grabber/Services/Media/ImageMetadata.swift
+++ Frame Grabber/Services/Media/ImageMetadata.swift
@@ -16,14 +16,23 @@ struct ImageMetadata {
 
 extension ImageMetadata {
     
-    static func metadata(for creationDate: Date?, location: CLLocation?) -> ImageMetadata {
+    static func metadata(
+        forCreationDate date: Date? = nil,
+        location: CLLocation? = nil,
+        make: String? = nil,
+        model: String? = nil,
+        software: String? = nil,
+        userComment: String? = nil
+    ) -> ImageMetadata {
+        
         var properties = Properties()
 
-        if let date = creationDate {
-            properties[kCGImagePropertyExifDictionary] = exifProperties(for: date)
-            properties[kCGImagePropertyTIFFDictionary] = tiffProperties(for: date)
-        }
-
+        let exif = exifProperties(forCreationDate: date, userComment: userComment)
+        let tiff = tiffProperties(forCreationDate: date, make: make, model: model, software: software)
+                
+        properties.setIfNotNil(kCGImagePropertyExifDictionary, exif.isEmpty ? nil : exif)
+        properties.setIfNotNil(kCGImagePropertyTIFFDictionary, tiff.isEmpty ? nil : tiff)
+        
         if let location = location {
             properties[kCGImagePropertyGPSDictionary] = gpsProperties(for: location)
         }
@@ -31,14 +40,37 @@ extension ImageMetadata {
         return ImageMetadata(properties: properties)
     }
 
-    static func exifProperties(for creationDate: Date) -> Properties {
-        let exifDateString = DateFormatter.exifDateTimeFormatter().string(from: creationDate)
-        return [kCGImagePropertyExifDateTimeOriginal: exifDateString as CFString]
+    static func exifProperties(
+        forCreationDate date: Date? = nil,
+        userComment: String? = nil
+    ) -> Properties {
+        
+        var properties = Properties()
+        let exifDate = date.flatMap(DateFormatter.exifDateTimeFormatter().string)
+            
+        properties.setIfNotNil(kCGImagePropertyExifDateTimeOriginal, exifDate as CFString?)
+        properties.setIfNotNil(kCGImagePropertyExifDateTimeDigitized, exifDate as CFString?)
+        properties.setIfNotNil(kCGImagePropertyExifUserComment, userComment as CFString?)
+            
+        return properties
     }
 
-    static func tiffProperties(for creationDate: Date) -> Properties {
-        let exifDateString = DateFormatter.exifDateTimeFormatter().string(from: creationDate)
-        return [kCGImagePropertyTIFFDateTime: exifDateString as CFString]
+    static func tiffProperties(
+        forCreationDate date: Date? = nil,
+        make: String? = nil,
+        model: String? = nil,
+        software: String? = nil
+    ) -> Properties {
+        
+        var properties = Properties()
+        let exifDate = date.flatMap(DateFormatter.exifDateTimeFormatter().string)
+        
+        properties.setIfNotNil(kCGImagePropertyTIFFDateTime, exifDate as CFString?)
+        properties.setIfNotNil(kCGImagePropertyTIFFMake, make as CFString?)
+        properties.setIfNotNil(kCGImagePropertyTIFFModel, model as CFString?)
+        properties.setIfNotNil(kCGImagePropertyTIFFSoftware, software as CFString?)
+        
+        return properties
     }
 
     static func gpsProperties(for location: CLLocation) -> Properties {
@@ -72,5 +104,14 @@ private extension DateFormatter {
         formatter.timeZone = TimeZone(abbreviation: "UTC")
         formatter.dateFormat = "yyyy:MM:dd HH:mm:ss"
         return formatter
+    }
+}
+
+// MARK: - Util
+
+private extension Dictionary {
+    mutating func setIfNotNil(_ key: Key, _ value: Value?) {
+        guard let value = value else { return }
+        self[key] = value
     }
 }
