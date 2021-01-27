17d2321ec659356852cba6df632b5addba848cff
Arthur Hammer <hi@arthurhammer.de>
2021-01-27 21:56:56 +0100

    Increase contrast for filter button

--- Frame Grabber/Album/AlbumViewSettingsButton.swift
+++ Frame Grabber/Album/AlbumViewSettingsButton.swift
@@ -8,17 +8,33 @@ class AlbumViewSettingsButton: UIButton {
         super.awakeFromNib()
         configureViews()
     }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        if previousTraitCollection?.hasDifferentColorAppearance(comparedTo: traitCollection) == true {
+            updateViews()
+        }
+    }
 
     private func configureViews() {
-        clipsToBounds = true
+        clipsToBounds = false
         layer.cornerRadius = Style.buttonCornerRadius
         layer.cornerCurve = .continuous
 
         titleLabel?.font = .preferredFont(forTextStyle: .subheadline, weight: .semibold)
         titleLabel?.adjustsFontForContentSizeCategory = true
 
-        backgroundColor = UIColor.systemGray5.withAlphaComponent(0.95)
         setImage(UIImage(systemName: "line.horizontal.3.decrease.circle"), for: .normal)
+        backgroundColor = UIColor.systemBackground.withAlphaComponent(0.95)
+        applyToolbarShadow()
+        
+        updateViews()
+    }
+    
+    private func updateViews() {
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.systemGray6.cgColor
     }
     
     /// Adds the receiver to the given view and sets up constraints.
