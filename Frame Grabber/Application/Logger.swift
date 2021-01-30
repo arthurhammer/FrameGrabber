7f7ea64f5acf5b1a4056cd5b0e5e32b696f0847b
Arthur Hammer <hi@arthurhammer.de>
2021-01-30 16:33:28 +0100

    Add logger to app target

---
+++ Frame Grabber/Application/Logger.swift
@@ -0,0 +1,10 @@
+import os.log
+import Foundation
+
+@available(iOS 14.0, *)
+extension Logger {
+
+    private static let subsystem = Bundle.main.bundleIdentifier ?? "de.arthurhammer.FrameGrabber"
+
+    static let app: Logger? = Logger(subsystem: subsystem, category: "app")
+}
