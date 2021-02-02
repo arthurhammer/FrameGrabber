350143de8e6a6b15f55d2fe9d033a3478ce56bed
Arthur Hammer <hi@arthurhammer.de>
2021-02-02 20:58:46 +0100

    Allow Combine sink assigning to optional

--- Frame Grabber/Utilities/Combine.swift
+++ Frame Grabber/Utilities/Combine.swift
@@ -2,7 +2,7 @@ import Combine
 
 extension Publisher where Self.Failure == Never {
 
-    func assignWeak<Root>(to keyPath: ReferenceWritableKeyPath<Root, Self.Output>, on object: Root) -> AnyCancellable where Root: AnyObject {
+    func assignWeak<Root>(to keyPath: ReferenceWritableKeyPath<Root, Self.Output>, on object: Root?) -> AnyCancellable where Root: AnyObject {
         sink { [weak object] value in
             object?[keyPath: keyPath] = value
         }
