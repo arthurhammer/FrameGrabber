cdbbbb29b778b9fb8f40a7c88f1dff7db3bcf4f1
Arthur Hammer <hi@arthurhammer.de>
2021-11-29 23:00:30 +0100

    Re-enable logging

--- Frame Grabber/Application/Logger.swift
+++ Frame Grabber/Application/Logger.swift
@@ -1,10 +1,9 @@
-//import os.log
-//import Foundation
-//
-//@available(iOS 14.0, *)
-//extension Logger {
-//
-//    private static let subsystem = Bundle.main.bundleIdentifier ?? "de.arthurhammer.FrameGrabber"
-//
-//    static let app: Logger? = Logger(subsystem: subsystem, category: "app")
-//}
+import os.log
+import Foundation
+
+extension Logger {
+
+    private static let subsystem = Bundle.main.bundleIdentifier ?? "de.arthurhammer.FrameGrabber"
+
+    static let app: Logger? = Logger(subsystem: subsystem, category: "app")
+}
