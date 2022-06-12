99e9d48816f57baed13a1a14773fedd6aada0b25
Arthur Hammer <hi@arthurhammer.de>
2022-06-12 17:23:27 +0200

    Use hierarchical chevron icon for title button

--- Frame Grabber/Scenes/Library/LibraryViewController.swift
+++ Frame Grabber/Scenes/Library/LibraryViewController.swift
@@ -90,7 +90,24 @@ final class LibraryViewController: UIViewController {
     private func configureTitleButton() {
         navigationItem.titleView = UIView()
         titleButton.configureDynamicTypeLabel()
+        titleButton.configureTrailingAlignedImage()
+        titleButton.setImage(titleButtonImage(), for: .normal)
         updateTitleButton()
+    }
+    
+    private func titleButtonImage() -> UIImage? {
+        if #available(iOS 15, *) {
+            let configuration = UIImage.SymbolConfiguration(paletteColors: [.accent, .secondarySystemFill])
+                .applying(UIImage.SymbolConfiguration(pointSize: 26, weight: .bold))
+                .applying(UIImage.SymbolConfiguration(scale: .small))
+            
+            return UIImage(systemName: "chevron.down.circle.fill", withConfiguration: configuration)
+        } else {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 18, weight: .bold)
+                .applying(UIImage.SymbolConfiguration(scale: .small))
+            
+            return UIImage(systemName: "chevron.down", withConfiguration: configuration)
+        }
     }
     
     private func updateTitleButton() {
