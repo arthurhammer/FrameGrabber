4f280c01f838139d61fcbd40a56688c320675dcd
Arthur Hammer <hi@arthurhammer.de>
2023-05-13 18:33:41 +0200

    Add location parsing for iOS 15

--- Frame Grabber/Services/Metadata/ISO6709LocationParser.swift
+++ Frame Grabber/Services/Metadata/ISO6709LocationParser.swift
@@ -14,6 +14,35 @@ struct ISO6709LocationParser {
     ///
     /// - SeeAlso: https://en.wikipedia.org/wiki/ISO_6709
     func location(from string: String) -> CLLocation? {
+        let components = components(from: string)
+        
+        guard let latitude = components?.latitude, let longitude = components?.longitude else {
+            return nil
+        }
+        
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+
+        return CLLocation(
+            coordinate: coordinate,
+            altitude: components?.altitude ?? 0,
+            horizontalAccuracy: -1,
+            verticalAccuracy: -1,
+            timestamp: Date()
+        )
+    }
+    
+    typealias LocationComponents = (latitude: Double, longitude: Double, altitude: Double?)
+    
+    private func components(from string: String) -> LocationComponents? {
+        if #available(iOS 16.0, *) {
+            return regexComponents(from: string)
+        } else {
+            return legacyComponents(from: string)
+        }
+    }
+    
+    @available(iOS 16.0, *)
+    private func regexComponents(from string: String) -> LocationComponents? {
         let pattern = /(?<latitude>[+-][0-9.]+)(?<longitude>[+-][0-9.]+)(?<altitude>[+-][0-9.]+)?/
         let matches = string.matches(of: pattern)
         
@@ -23,17 +52,46 @@ struct ISO6709LocationParser {
         else {
             return nil
         }
-                
-        let altitudeString = groups.altitude
-        let altitude = altitudeString.flatMap(Double.init)
-        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let altitude = groups.altitude.flatMap(Double.init)
+        return (latitude, longitude, altitude)
+    }
+    
+    @available(iOS, obsoleted: 16)
+    private func legacyComponents(from string: String) -> LocationComponents? {
+        let pattern = "([+-][0-9.]+)([+-][0-9.]+)([+-][0-9.]+)?"
+        let groups = string.captureGroups(matching: pattern)
+        
+        guard let latitudeString = groups[safe: 1],
+              let longitudeString = groups[safe: 2],
+              let latitude = Double(latitudeString),
+              let longitude = Double(longitudeString) else { return nil }
+        
+        let altitude = groups[safe: 3].flatMap(Double.init)
+        return (latitude, longitude, altitude)
+    }
+}
 
-        return CLLocation(
-            coordinate: coordinate,
-            altitude: altitude ?? 0,
-            horizontalAccuracy: -1,
-            verticalAccuracy: -1,
-            timestamp: Date()
-        )
+// MARK: - Utilities
+
+extension String {
+    @available(iOS, obsoleted: 16)
+    fileprivate func captureGroups(matching pattern: String) -> [String] {
+        let fullRange = NSRange(location: 0, length: count)
+        
+        guard let regex = try? NSRegularExpression(pattern: pattern),
+              let result = regex.firstMatch(in: self, range: fullRange) else { return [] }
+        
+        return (0..<result.numberOfRanges)
+            .map(result.range(at:))
+            .compactMap { Range($0, in: self) }
+            .map { String(self[$0]) }
+    }
+}
+
+extension Collection {
+    @available(iOS, obsoleted: 16)
+    fileprivate subscript(safe index: Index) -> Element? {
+        indices.contains(index) ? self[index] : nil
     }
 }
